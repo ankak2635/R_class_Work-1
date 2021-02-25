dfd2d5178bfd1286158ad6824b8088fd1fb64a28
ankak2635 <78867045+ankak2635@users.noreply.github.com>
2021-02-25 16:07:46 +0530

    Create R class 1.R

---
+++ R class 1.R
@@ -0,0 +1,40 @@
+print("Hello, World!")
+2 + 3
+2304 / 233
+b <- 55
+a = 63
+a = b
+(a * 55)
+ x = c(b,45," ")
+typeof(x)
+x <- c(2,4,5, "ank")
+typeof(x)
+nummy <- c("5","7","4")
+cast <- as.numeric(nummy)
+(nummy)
+typeof(cast)
+cast2<- as.logical(cast)
+(cast2)
+
+
+
+nummy_int <- c(1L,2L,3L)
+typeof(nummy)
+typeof(nummy_int)
+is.numeric(nummy_int)
+is.numeric(nummy)
+is.double(nummy)
+types <- c("int","double","character")
+typeof(types)
+logicals <- c(TRUE,F,TRUE,T, FALSE)
+c (5,6, TRUE )
+cast<- as.character(logicals)
+typeof(cast)
+is.numeric(types)
+length(types)
+month.abb
+month.abb[seq(7,12,1)]
+odd <- c(1,3,5,7,9)
+even <- c(2,4,6,8)
+num <- c(odd,even)
+(num)
